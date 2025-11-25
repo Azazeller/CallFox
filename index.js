80eacd78c6a150e19a96499af6318b2007bef1c5
Azazeller <34882115+Azazeller@users.noreply.github.com>
2025-11-25 05:50:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -261,3 +261,4 @@ app.post("/webhook", async (req, res) => {
 ============================================================ */
 app.listen(3000, () => {
   console.log("Bot running on port 3000");
+   });
